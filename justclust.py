9e8f47b32ebc7e5ce5b92bd926e8b1e991a3a271
Lithakazi Nxiweni <L.Nxiweni@tuks.co.za>
2016-05-11 20:32:44 +0200

    generating html file

--- justclust.py
+++ justclust.py
@@ -7,6 +7,8 @@ from configparser import ConfigParser
 import os
 from string import punctuation
 import pandas as pd
+import re
+
 
 # read config file
 config = ConfigParser()
@@ -35,6 +37,12 @@ def tokenize(text):
 def strip_punctuation(s):
     return''.join(c for c in s if c not in punctuation)
 
+def remove_digits(s):
+    return''.join(i for i in s if not i.isdigit())
+
+def remove_shortwords(s):
+    return' '.join(word for word in anytext.split() if len(word)>3)
+
 titles = []
 titlespdf = []
 for (subdir, dirs, files) in os.walk(direct):
@@ -44,8 +52,23 @@ for (subdir, dirs, files) in os.walk(direct):
             shakes = open(file_path, 'r', encoding='utf-8')
             text = shakes.read()
             no_punctuation = strip_punctuation(text)
-            token_dict[f] = no_punctuation
+            no_digits = remove_digits(no_punctuation)
+            new_text = remove_digits(no_digits)
+            token_dict[f] = new_text
             titles.append(f)
+            tokens = word_tokenize(new_text)
+            stems = stem_tokens(tokens, stemmer)
+
+from collections import Counter
+count = Counter(tokens)
+count2 = dict(count)
+stemdict = {}
+for i in range(len(stems)):
+    key = stems[i]
+    temp = tokens[i],count2[tokens[i]]
+    stemdict.setdefault(key,[])
+    if temp not in stemdict[key]:
+        stemdict[key].append(temp)
 
 true_k =10
 
@@ -67,20 +90,55 @@ print("For n_clusters =", true_k,
           "The average silhouette_score is :", silhouette_avg)
 
 
-# print("Top terms per cluster:")
-# order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-# terms = vectorizer.get_feature_names()
-# for i in range(true_k):
-#     print("Cluster %d:" % i, end='')
-#     for ind in order_centroids[i, :10]:
-#         print(' %s' % terms[ind], end='')
-#     print()
-#
+print("Top terms per cluster:")
+order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+
+for i in range(true_k):
+    print("Cluster %d:" % i, end='')
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind], end='')
+    print()
 
 
 clusterlist = km.labels_.tolist()
 # print(clusterlist)
 documents = {'title': titles, 'cluster': clusterlist}
-frame = pd.DataFrame(documents, index = [clusterlist] , columns = ['title', 'cluster'])
+frame = pd.DataFrame(documents, index=[clusterlist], columns= ['title', 'cluster'])
 
 frame.to_csv('summary of clusters.csv')
+
+with open("%s Cluster.html" %true_k, "w") as htmlfile:
+    htmlfile.write('<table border="1" cellpadding="3">')
+    for true_k in range(true_k):
+        print('cluster %d' %(true_k+1))
+        # htmlfile.write()
+
+        # table summarizing the top terms per cluster
+        htmlfile.write('<tr><td><a href="Cluster %d"'%(true_k+1))
+        htmlfile.write('title="Click to view list of documents in cluster %d"'%(true_k+1))
+        htmlfile.write('>cluster %d</a></td>' %(true_k+1))
+        htmlfile.write('<td>')
+
+        for ind in order_centroids[true_k, :10]:
+            a = terms[ind]
+            htmlfile.write(' %s, ' %a)
+        htmlfile.write('</td>')
+
+
+    htmlfile.write('</table>')
+
+    for true_k in range(true_k):
+
+
+        # Generate list of clusters
+        htmlfile.write('<h4><a name="cluster%d">' %(true_k+1))
+        htmlfile.write('Cluster %d</a></h4><br />' %(true_k+1))
+        for index, row in frame.iterrows():
+            if row['cluster'] == true_k:
+                base, ext = os.path.splitext(row['title'])
+                htmlfile.write('<ol >')
+                htmlfile.write('<a href = %s.pdf>' %base)
+                htmlfile.write('%s' %base)
+                htmlfile.write('</a>')
+                htmlfile.write('</ol>')
